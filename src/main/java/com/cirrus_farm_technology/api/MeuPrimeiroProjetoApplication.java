f46265dbd90be8d1a479ae2ae91ffc6a635f6990
vinaxa <vinaxa@gmail.com>
2020-04-24 20:29:18 -0300

    Atualização via eclipse

--- src/main/java/com/cirrus_farm_technology/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/cirrus_farm_technology/api/MeuPrimeiroProjetoApplication.java
@@ -8,7 +8,7 @@ public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-		System.out.println("Meu primeiro projeto...");
+		System.out.println("Meu primeiro projeto!!!");
 	}
 
 }
